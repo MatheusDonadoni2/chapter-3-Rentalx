b6abd3a3a7e86aa8a84ed0dcb88c25272bc809fe
MatheusDonadoni2 <62766278+MatheusDonadoni2@users.noreply.github.com>
2022-05-15 21:48:20 -0300

    up
    up
    
    up
    
    up
    
    up
    
    uop
    
    Criando repositório de especificações

---
+++ src/modules/cars/services/CreateCategoryService.ts
@@ -0,0 +1,18 @@
+import { ICategoriesRepository } from '../repositories/ICategoryRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+class CreateCategoryService {
+  constructor(private categoriesRepository: ICategoriesRepository) { }
+  execute({ description, name }: IRequest): void {
+    const categoryAlreadyExist = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExist) {
+      throw new Error('Category already exists.');
+    }
+    this.categoriesRepository.create({ name, description });
+  }
+}
+export { CreateCategoryService };
