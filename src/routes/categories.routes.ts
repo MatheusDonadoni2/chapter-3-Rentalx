46d61e1b3081eced752e85caa015990642a11cb7
matheusdonadoni2 <matheusgoncalvesdonadoni@gmail.com>
2022-06-04 16:39:45 -0300

    .

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,19 +7,19 @@ import { listCategoriesController } from '../modules/cars/useCases/listCategorie
 
 const categoriesRoutes = Router();
 const upload = multer({
-  dest: './tmp',
+    dest: './tmp',
 });
 
 categoriesRoutes.post('/', (request, response) => {
-  return createCategoryController.handle(request, response);
+    return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
-  return listCategoriesController.handle(request, response);
+    return listCategoriesController.handle(request, response);
 });
 
 categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
-  return importCategoryController.handle(request, response);
+    return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
